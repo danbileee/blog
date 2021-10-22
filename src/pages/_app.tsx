a095876762bd1ef2655840997eb80ce1423f549c
Danbi Lee <blocktobody@gmail.com>
2021-10-22 22:06:53 +0900

    feat: yarn 버전 업그레이드

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,20 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import { GlobalStyle } from '../styles';
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Danbi Lee | blog</title>
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp;
+export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
 import { NextPage } from 'next';
 
-import { GlobalStyle } from '../styles';
-
 const Home: NextPage = () => (
   <div>
-    <GlobalStyle />
     <div>Hello World</div>
   </div>
 );
