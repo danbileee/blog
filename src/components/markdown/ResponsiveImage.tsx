c11db0df7b2391e5311e57c11715c2d9418649f8
Danbi Lee <hello@danbileee.com>
2025-01-15 23:06:21 +0900

    FIX: 마이너 스타일 수정

--- src/components/markdown/ResponsiveImage.tsx
+++ src/components/markdown/ResponsiveImage.tsx
@@ -8,6 +8,7 @@ type Props = Omit<ImgHTMLAttributes<HTMLImageElement>, 'placeholder'>;
 export default function ResponsiveImage({ src, ...props }: Props) {
   const [alt, size] = props?.alt?.split('|') ?? [];
 
+  // TODO: 이미지 전체보기 모달 구현
   return (
     <Figure size={size}>
       <Image src={src ?? ''} layout="fill" placeholder="empty" alt={alt} {...props} />
